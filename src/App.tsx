b5a46a028799b5f699751de7cad99a8213c87f63
Daphnee Jeune <daphneejeune.dev@gmail.com>
2025-04-01 12:08:43 -0500

    start working on card

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
-    <div className='text-red-500'>
-      Hello
+    <div className="text-xl border rounded-lg px-2 m-2 bg-gray-50">
+      <div className="text-base font-semibold py-2">UI engineer L5</div>
+      <div className="flex py-2 gap-4 justify-between">
+        <div>Job offer</div>
+        <div>3</div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
