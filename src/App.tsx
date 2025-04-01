ba5b281146d46b2c152d6a40aaa1621fce0e7c87
Daphnee Jeune <daphneejeune.dev@gmail.com>
2025-04-01 15:20:26 -0500

    correctly align total points with h2

--- src/App.tsx
+++ src/App.tsx
@@ -22,10 +22,15 @@ function App() {
     <div className="flex">
       {columns.map((column, i) => (
         <div key={i} className="">
-          <h2 className="text-3xl p-2 capitalize font-bold text-gray-500">
-            {column.status}
-          </h2>
-          {column.tasks.reduce((total, task) => total + (task?.points ?? 0), 0)}
+          <div className="flex justify-between text-3xl p-2 font-bold text-gray-500">
+            <h2 className="capitaliz">{column.status}</h2>
+            <div>
+              {column.tasks.reduce(
+                (total, task) => total + (task?.points ?? 0),
+                0
+              )}
+            </div>
+          </div>
           {column.tasks.map((task) => (
             <TaskCard
               key={task.id}
