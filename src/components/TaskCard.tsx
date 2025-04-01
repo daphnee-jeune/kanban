7003537b39e282b956b7a603826e273d55e12146
Daphnee Jeune <daphneejeune.dev@gmail.com>
2025-04-01 15:01:00 -0500

    extract icons into util file

--- src/components/TaskCard.tsx
+++ src/components/TaskCard.tsx
@@ -1,61 +1,6 @@
 import { useState } from "react";
 import { Task } from "../utils/data-tasks";
-
-const lowPriorityIcon = (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    className="h-6 w-6 text-blue-400"
-    fill="none"
-    viewBox="0 0 24 24"
-    stroke="currentColor"
-  >
-    <path
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      strokeWidth={2}
-      d="M5 9l7 7 7-7"
-    />
-  </svg>
-);
-const mediumPriorityIcon = (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    className="h-6 w-6 text-yellow-500"
-    fill="none"
-    viewBox="0 0 24 24"
-    stroke="currentColor"
-  >
-    <path
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      strokeWidth={2}
-      d="M5 10h14"
-    />
-    <path
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      strokeWidth={2}
-      d="M5 14h14"
-    />
-  </svg>
-);
-
-const highPriorityIcon = (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    className="h-6 w-6 text-red-500"
-    fill="none"
-    viewBox="0 0 24 24"
-    stroke="currentColor"
-  >
-    <path
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      strokeWidth={2}
-      d="M5 15l7-7 7 7"
-    />
-  </svg>
-);
+import { lowPriorityIcon, mediumPriorityIcon, highPriorityIcon } from "../utils/icons";
 
 const TaskCard = ({
   task: { title, id, points, priority },
---
+++ src/utils/icons.tsx
@@ -0,0 +1,55 @@
+export const lowPriorityIcon = (
+ <svg
+   xmlns="http://www.w3.org/2000/svg"
+   className="h-6 w-6 text-blue-400"
+   fill="none"
+   viewBox="0 0 24 24"
+   stroke="currentColor"
+ >
+   <path
+     strokeLinecap="round"
+     strokeLinejoin="round"
+     strokeWidth={2}
+     d="M5 9l7 7 7-7"
+   />
+ </svg>
+);
+export const mediumPriorityIcon = (
+ <svg
+   xmlns="http://www.w3.org/2000/svg"
+   className="h-6 w-6 text-yellow-500"
+   fill="none"
+   viewBox="0 0 24 24"
+   stroke="currentColor"
+ >
+   <path
+     strokeLinecap="round"
+     strokeLinejoin="round"
+     strokeWidth={2}
+     d="M5 10h14"
+   />
+   <path
+     strokeLinecap="round"
+     strokeLinejoin="round"
+     strokeWidth={2}
+     d="M5 14h14"
+   />
+ </svg>
+);
+
+export const highPriorityIcon = (
+ <svg
+   xmlns="http://www.w3.org/2000/svg"
+   className="h-6 w-6 text-red-500"
+   fill="none"
+   viewBox="0 0 24 24"
+   stroke="currentColor"
+ >
+   <path
+     strokeLinecap="round"
+     strokeLinejoin="round"
+     strokeWidth={2}
+     d="M5 15l7-7 7 7"
+   />
+ </svg>
+);
