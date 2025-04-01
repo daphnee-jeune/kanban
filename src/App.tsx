88c05cad4ca6aecdc9841c40c46edcd9618c063a
Daphnee Jeune <daphneejeune.dev@gmail.com>
2025-04-01 12:17:54 -0500

    implement taskcard

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
 import "./App.css";
+import TaskCard from "./components/TaskCard";
 
 function App() {
   return (
-    <div className="text-xl border rounded-lg px-2 m-2 bg-gray-50">
-      <div className="text-base font-semibold py-2">UI engineer L5</div>
-      <div className="flex py-2 gap-4 justify-between">
-        <div>Job offer</div>
-        <div>3</div>
-      </div>
-    </div>
+    <>
+      <TaskCard title='UI Engineer L5' id={3} points={'4'} />
+    </>
   );
 }
 
---
+++ src/components/TaskCard.tsx
@@ -0,0 +1,19 @@
+type TaskCardProps = {
+ title: string;
+ id: number;
+ points: string;
+}
+
+const TaskCard = ({ title, id, points }: TaskCardProps) => {
+  return (
+    <div className="text-xl border rounded-lg px-2 m-2 bg-gray-50">
+      <div className="text-base font-semibold py-2">{title}</div>
+      <div className="flex py-2 gap-4 justify-between">
+        <div>{id}</div>
+        <div>{points}</div>
+      </div>
+    </div>
+  );
+};
+
+export default TaskCard;
